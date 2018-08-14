02581051983189e3486a9ba759750c47b083ce7c
Nikunj Prajapati <nikunjd.np@gmail.com>
2018-08-14 18:22:53 +0530

    comment removed

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-var express = require('express');
-var app = express();
-var controller = require('./controller');
-var bodyParser = require('body-parser');
-var allowCrossDomain = function(req, res, next) {
+let express = require('express');
+let app = express();
+let controller = require('./controller');
+let bodyParser = require('body-parser');
+let allowCrossDomain = function(req, res, next) {
 
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
@@ -10,26 +10,19 @@ var allowCrossDomain = function(req, res, next) {
     if (req.method == 'OPTIONS') {
         res.status(200).end();
     } else {
-        console.log("------------------Req URL-----------------");
-        console.log("Req.URL :: ", req.path);
-        console.log("------------------Req method-----------------");
-        console.log("Req.params :: ", req.method);
-        console.log("--------------------------------------------");
         next();
     }
-    // next();
 };
 app.use(allowCrossDomain);
 app.use(bodyParser.json({
     limit: '50mb'
 }));
-//app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     limit: '50mb',
     extended: true,
     parameterLimit: 100000000
 }));
-var server = app.listen(6001, function() {
+let server = app.listen(6001, function() {
     controller.indentify_node_process.start();
     console.log('Server listening on port 6001');
 });
